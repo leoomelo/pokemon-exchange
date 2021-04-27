7defa4899a479e3d367dff4670d097dcb18a9b4a
Leonardo Melo <leonardo_melo@apple.com>
2021-04-27 16:27:56 -0300

    create methdos to save and find

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+const { saveFairExchange, findAll } = require('./db/db')
 const app = express()
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json())
 app.use(cors());
-
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/test', (req, res) => {
+  res.json('Hello World!')
+})
+
+app.get('/exchanges', async (req, res) => {
+  findAll()
+    .then(exchanges => res.json(exchanges))
+})
+
+app.post('/fairExchange', (req, res) => {
+  const body = req.body
+  saveFairExchange(body)
+  res.json(body)
 })
 
 app.listen(PORT, () => {
